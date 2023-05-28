7289bc8c1308c7e6bf48bfad66aeb274035c1b37
hunte <besscomdark@gmail.com>
2023-05-28 22:06:18 +0800

    調整

--- Assets/Scripts/test/ApplyForceAtOffet.cs
+++ Assets/Scripts/test/ApplyForceAtOffet.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class ApplyForceAtOffet : MonoBehaviour
 {
+    [Header("力量")]
     public float force = 100.0f;
+    [Header("力量方向")]
     public Vector3 forceDirection = Vector3.forward;
 
-    // 腳本簡潔但不能確定方向
-
+    [Header("按鈕名稱")]
     public string buttonName = "Fire1";
+    [Header("偏移路徑")]
     public Vector3 offset;
 
 
     void Update()
     {
+        // 輸入按鈕
         if (Input.GetButton(buttonName))
         {
-
+            // 取得剛體.添加推力方位(力量方向.歸一化 * 力量, 方位 + 偏移)
             GetComponent<Rigidbody>().AddForceAtPosition(forceDirection.normalized * force, transform.position + offset);
         }
         else
         {
+            // 取得剛體.添加推力方位(力量方向.歸一化 * -力量, 方位 + 偏移)
             GetComponent<Rigidbody>().AddForceAtPosition(forceDirection.normalized * -force, transform.position + offset);
         }
     }
---
+++ Assets/Scripts/test/ShootBall.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBall : MonoBehaviour
+{
+    [Header("¤O¶q")]
+    public float force = 100.0f;
+    public string buttonName = "Fire1";
+
+    private List<Rigidbody> ballList = new List<Rigidbody>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown(buttonName))
+        {
+            foreach (Rigidbody ball in ballList)
+            {
+                ball.AddForce(Vector3.forward * force);
+            }
+        }
+    }
+
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Object Entered");
+        if (col.GetComponent<Rigidbody>())
+        {
+            Debug.Log("Object Has Rigidbody");
+            ballList.Add(col.GetComponent<Rigidbody>());
+        }
+    }
+
+
+
+    void OnTriggerExit(Collider col)
+    {
+        Debug.Log("Object Exited");
+        if (col.GetComponent<Rigidbody>())
+        {
+            Debug.Log("Object Has Rigidbody");
+            ballList.Remove(col.GetComponent<Rigidbody>());
+        }
+    }
+}
