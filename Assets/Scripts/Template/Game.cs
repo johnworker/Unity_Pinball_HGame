4def1c32f74161dfdcf08ed471538e722dd7a034
hunte <besscomdark@gmail.com>
2023-06-05 22:50:41 +0800

    添加註解

--- Assets/Scripts/Template/Game.cs
+++ Assets/Scripts/Template/Game.cs
@@ -5,27 +5,42 @@ using UnityEngine.UI;
 using TMPro;
 
 public class Game : MonoBehaviour {
+	// instance：這是一個靜態屬性，用於存儲 Game 類別的唯一實例。
+	// 可以通過 Game.instance 來訪問這個實例。
 	public static Game instance { get; private set; }
 
+	[Header("遊戲是否處於活動狀態")]
 	public bool isActive = false;
+	[Header("彈珠是否準備好射擊")]
 	public bool ballReady = true;
+	[Header("遊戲中剩餘的彈珠數量")]
 	public int ballCount = 3;
+	[Header("遊戲是否處於活動狀態")]
 	public TextMeshProUGUI ballCountText;
 
-	[Header("Game UI")]
+	[Header("遊戲 UI")]
+	[Header("遊戲中的選單")]
 	public RectTransform menuPanel;
+	[Header("遊戲畫面")]
 	public RectTransform gamePanel;
+	[Header("結束畫面")]
 	public RectTransform endPanel;
 
-	[Header("Ball")]
+	[Header("彈珠")]
+	[Header("初始彈珠數量")]
 	public int ballStartCount = 3;
+	[Header("用於顯示彈珠的圖像或模型")]
 	public GameObject ballDisplay;
+	[Header("彈珠的預製物件")]
 	public Ball ballPrefab;
+	[Header("彈珠生成的位置")]
 	public Transform ballSpawnPoint;
 
+	// 管理遊戲的分數
 	public GameScore score { get; private set; }
+	// 用於控制遊戲的行為
 	public GameController controller { get; private set; }
-
+	[Header("表示其他的 Canvas（畫布）")]
 	public GameObject otherCanvas;
 
 	void Awake() {
@@ -34,6 +49,19 @@ public class Game : MonoBehaviour {
 		score = GetComponent<GameScore>();
 		controller = GetComponent<GameController>();
 	}
+
+	/*在 Awake() 方法中，首先將 instance 屬性設置為當前的 Game 實例。
+	 * 這樣可以在其他地方通過 Game.instance 訪問這個實例。
+
+	 *接下來，使用 GetComponent<T>() 方法，從當前物件上獲取 GameScore 組件並將其賦值給 score 屬性。
+	 *這意味著當前的 Game 物件具有一個 GameScore 組件，並且可以通過 score 屬性來訪問該組件。
+
+	 *同樣地，使用 GetComponent<T>() 方法，從當前物件上獲取 GameController 組件並將其賦值給 controller 屬性。
+	 *這意味著當前的 Game 物件具有一個 GameController 組件，並且可以通過 controller 屬性來訪問該組件。
+
+	 *總結來說，Awake() 方法在遊戲物件被創建時被調用，它將設置 Game 類別的 instance 屬性、獲取並設置 GameScore 和 GameController 組件的參考。
+	 *這樣可以在遊戲運行期間輕鬆地訪問這些組件並執行相關的操作。
+	 */
 
 	void Start() {
 		isActive = false;
@@ -47,6 +75,17 @@ public class Game : MonoBehaviour {
 
 		otherCanvas.SetActive(true);
 	}
+	/*
+	 * 在 Start() 方法中，首先將 isActive 屬性設置為 false，表示遊戲目前不處於活動狀態。
+	 * 接下來，調用 SetBallReady(false) 方法，將 ballReady 屬性設置為 false，表示彈珠目前不準備射擊。
+	 * 然後，通過設置 menuPanel、gamePanel 和 endPanel 的 gameObject.SetActive(true/false)，來顯示或隱藏對應的 UI 面板。
+	 * 這裡將 menuPanel 設置為顯示，而 gamePanel 和 endPanel 設置為隱藏。
+	 * 最後，將 otherCanvas 的 SetActive(true) 設置為啟用，以顯示其他的 Canvas（畫布）。
+
+	 總結來說，Start() 方法在遊戲物件啟用時被調用，它設置了遊戲的初始狀態，包括設置遊戲為非活動狀態、
+	 彈珠不準備射擊、顯示相應的 UI 面板以及啟用其他的 Canvas。
+	 這些設置可以在遊戲開始時呈現正確的初始畫面和狀態。
+	*/
 
 	/// <summary>
 	/// 開始遊戲, 將套用正確的活動並顯示右側面板
--- Assets/Scripts/Template/GameController.cs
+++ Assets/Scripts/Template/GameController.cs
@@ -6,21 +6,39 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Game))]
 public class GameController : MonoBehaviour {
 
-	[Header("Flippers")]
+	/* 這裡使用了 RequireComponent 屬性，這是一個 Unity 引擎提供的特殊屬性。
+	 * 它可以確保附加到同一個 GameObject 上的類別具有指定的組件。
+	 * 在這個例子中，RequireComponent(typeof(Game)) 表示這個 GameController 類別需要附加一個 Game 組件。
+	 * 這個 Game 組件可能是一個自定義的組件，可能包含了遊戲邏輯、遊戲狀態管理等相關功能。
+	 * 這個 GameController 類別可能負責協調和控制整個遊戲的行為，並在遊戲運行時與 Game 組件進行交互。
+	 * 通過使用 RequireComponent 屬性，可以確保當你將 GameController 腳本附加到 GameObject 上時，它將自動附加一個 Game 組件。
+	 * 這有助於確保遊戲運行所需的相關組件始終存在並一起使用。
+	 
+	 * 總結來說，這個 GameController 類別是一個控制遊戲行為的類別，並要求附加一個 Game 組件。
+	 * 這個 Game 組件可能包含了遊戲的核心邏輯和狀態管理。 */
+
+	[Header("彈簧")]
 	public HingeJoint leftFlipper;
 	public HingeJoint rightFlipper;
+	[Header("彈簧活動角度")]
 	public float flipperActiveAngle = 45;
 
-	[Header("Ball")]
+	[Header("彈珠")]
+	[Header("力量範圍")]
 	public Vector2 minMaxForce = new Vector2(10, 20);
+	[Header("力量增益")]
 	public float powerGain = 1.2f;
+	[Header("UI 圖像")]
 	public Image powerImage;
 
+	// 用於儲存 Game 類別的實例
 	private Game game;
 
+	// 浮點數 力量
 	private float power;
 
 	void Awake() {
+		// 它會獲取這個腳本所附加的 GameObject 上的 Game 類別組件並將其賦值給 game 變數
 		game = GetComponent<Game>();
 	}
 
@@ -29,9 +47,15 @@ public class GameController : MonoBehaviour {
 		if (game.isActive) {
 			if (game.ballReady) {
 				ShootInput();
-				// StartCoroutine(AutoShootWait());
 			}
 		}
+		/*首先，它檢查 game.isActive 的值。
+		 * game 是之前在 Awake() 方法中賦值的 Game 類別的實例。
+		 * 如果 game.isActive 為真，則表示遊戲正在運行中。
+		 * 接下來，它檢查 game.ballReady 的值。
+		 * 這可能是一個布林（bool）型別的變數，用於檢查球是否準備好射擊。
+		 * 如果 game.ballReady 為真，則執行 ShootInput() 方法。
+		 * 該方法可能包含了處理射擊球的邏輯。*/
 
 		// 在主菜單中允許擋板
 		FlipperInput();
@@ -43,12 +67,18 @@ public class GameController : MonoBehaviour {
 	void FlipperInput() {
 		// 左彈簧
 		JointSpring leftSpring = leftFlipper.spring;
-		leftSpring.targetPosition = Input.GetKey(KeyCode.A) ? flipperActiveAngle : 0f;
+		/*使用條件運算符 ? 來根據按鍵輸入設置左彈簧的目標位置。
+		 * 如果按下 A 鍵或左箭頭鍵，則將目標位置設置為 flipperActiveAngle，否則設置為 0。
+		 */
+		leftSpring.targetPosition = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) ? flipperActiveAngle : 0f;
 		leftFlipper.spring = leftSpring;
 
 		// 右彈簧
 		JointSpring rightSpring = rightFlipper.spring;
-		rightSpring.targetPosition = Input.GetKey(KeyCode.D) ? flipperActiveAngle : 0f;
+		/*使用條件運算符 ? 來根據按鍵輸入設置右彈簧的目標位置。
+		 * 如果按下 A 鍵或右箭頭鍵，則將目標位置設置為 flipperActiveAngle，否則設置為 0。
+		 */
+		rightSpring.targetPosition = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) ? flipperActiveAngle : 0f;
 		rightFlipper.spring = rightSpring;
 	}
 
@@ -70,5 +100,23 @@ public class GameController : MonoBehaviour {
 		// 更新 UI（將自動夾在 0..1 之間）
 		powerImage.fillAmount = power;
 	}
-	
+
+	/*
+	 在這個方法中，有幾個邏輯判斷：
+	 
+	 首先，它檢查是否按下了空白鍵（KeyCode.Space）。
+	 如果是，則根據 powerGain 值和 Time.deltaTime（當前幀的時間間隔）來增加 power 變數的值。
+	 這段程式碼的目的是在按住空白鍵時增加力量值（power）。
+	 
+	 接下來，它檢查是否釋放了空白鍵。
+	 如果是，則根據 power 的值計算出射擊力量，並呼叫 game.ShootBall() 方法將球射出。
+	 射擊力量的計算是基於 minMaxForce 的範圍，並使用 Mathf.Clamp01() 方法將 power 限制在 0 到 1 之間。
+	 然後，使用線性插值來計算在 minMaxForce 範圍內的具體力量值，並將其作為參數傳遞給 game.ShootBall() 方法。
+
+	 最後，它將 power 的值用於更新 UI，具體來說是更新 powerImage 的 fillAmount 屬性。
+	 這個屬性表示填充圖像的比例，這裡使用 power 的值作為填充比例，因此當力量增加時，UI 會反映出力量的變化。
+
+	 總結來說，這個方法在按下和釋放空白鍵時管理射擊彈珠的邏輯。
+	 它追蹤並增加力量值，根據力量值計算射擊力量並觸發射擊行為，同時也更新了 UI 以反映力量的變化。
+	*/
 }
