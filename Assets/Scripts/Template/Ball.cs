4ba2646cfdd43336c365e676e5ce59a2d031460d
hunte <besscomdark@gmail.com>
2023-07-05 22:50:35 +0800

    調整材質

--- Assets/Scripts/Template/Ball.cs
+++ Assets/Scripts/Template/Ball.cs
@@ -7,17 +7,28 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 	public Rigidbody rigidBody { get; private set; }
 
+	public float speed = 5f;
+
 	void Awake() {
 		rigidBody = GetComponent<Rigidbody>();
 	}
 
-	/// <summary>
-	/// 給球加一個瞬間力
-	/// </summary>
-	/// <param name="force"></param>
-	public void AddForce(Vector3 force) 
+	private void Update()
+	{
+		// 根據剛體的速度更新球的旋轉
+		if (rigidBody.velocity != Vector3.zero)
+		{
+			transform.rotation = Quaternion.LookRotation(rigidBody.velocity.normalized);
+		}
+	}
+
+    /// <summary>
+    /// 給球加一個瞬間力
+    /// </summary>
+    /// <param name="force"></param>
+    public void AddForce(Vector3 force) 
 	{
 		rigidBody.AddForce(force, ForceMode.VelocityChange);
-		// 使rigidBody跟著移動旋轉
+
 	}
 }
