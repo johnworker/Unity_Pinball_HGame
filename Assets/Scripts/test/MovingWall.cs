947e7a78668f0a1531eab0ef5d95ce2ae8eb5acb
hunte <besscomdark@gmail.com>
2023-07-09 21:59:38 +0800

    場景物件設置

--- Assets/Scripts/test/MovingWall.cs
+++ Assets/Scripts/test/MovingWall.cs
@@ -2,35 +2,36 @@
 
 public class MovingWall : MonoBehaviour
 {
-    public float moveDistance = 5f;  // 墙壁移动的距离
-    public float moveSpeed = 2f;     // 墙壁移动的速度
-    public float interval = 10f;      // 移动的时间间隔
+    public float moveDistance = 5f;  // 牆壁移動的距離
+    public float moveSpeed = 2f;     // 牆壁移動的速度
+    public float interval = 10f;      // 移動的時間間隔
 
-    private float originalY;         // 原始的Y坐标
-    private float targetY;           // 目标的Y坐标
-    private float timer;             // 计时器
-    private bool movingUp = true;    // 是否向上移动
+    private float originalY;         // 原始的Y坐標
+    private float targetY;           // 目標的Y坐標
+    private float timer;             // 計時器
+    private bool movingUp = true;    // 是否向上移動
 
     private void Start()
     {
-        originalY = transform.position.y;  // 记录原始的Y坐标
-        targetY = (originalY + moveDistance) * moveSpeed * Time.deltaTime; // 设置初始目标位置为原始位置加上移动距离
+        originalY = transform.position.y;  // 記錄原始的Y坐標
+        targetY = (originalY + moveDistance) * moveSpeed * Time.deltaTime; // 設置初始目標位置為原始位置加上移動距離
     }
 
     private void Update()
     {
+        // 計時器隨著時間增加並依時間影格分配每個裝置
         timer += Time.deltaTime;
 
-        // 检查是否到达时间间隔
+        // 檢查是否到達時間間隔
         if (timer >= interval)
         {
             timer = 0f;
-            movingUp = !movingUp;  // 切换移动方向
+            movingUp = !movingUp;  // 切換移動方向
             // 下降要和上升距離一致
-            targetY = movingUp ? originalY - moveDistance / 1.1f : originalY + moveDistance ; // 根据移动方向更新目标位置
+            targetY = movingUp ? originalY - moveDistance / 1.1f : originalY + moveDistance; // 根據移動方向更新目標位置
         }
 
-        // 平滑移动墙壁
+        // 平滑移動牆壁
         float newY = Mathf.Lerp(transform.position.y, targetY, moveSpeed * Time.deltaTime);
         transform.position = new Vector3(transform.position.x, newY, transform.position.z);
 
