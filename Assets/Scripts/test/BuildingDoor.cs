034fa0c635651adc1a613c3d15c2c87a599d6b1b
hunte <besscomdark@gmail.com>
2023-07-10 22:39:09 +0800

    調整

---
+++ Assets/Scripts/test/BuildingDoor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BuildingDoor : MonoBehaviour
+{
+    public string nextSceneName;  // 下一個場景的名稱
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            // 如果碰撞體標籤為"Ball"（彈珠），則加載下一個場景
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
