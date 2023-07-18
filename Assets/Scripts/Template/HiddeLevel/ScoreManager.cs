69d1a69841c7bebad0a1eecd232a12d541a3fe27
johnworker <78348941+johnworker@users.noreply.github.com>
2023-07-18 20:39:13 +0800

    調整

---
+++ Assets/Scripts/Template/HiddeLevel/ScoreManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ScoreManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText; // 分數的UI文本
+
+    private int originalScore = 0;
+
+    private void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + originalScore.ToString();
+    }
+}
