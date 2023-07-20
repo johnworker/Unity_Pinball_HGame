de823d8cb6b68a2b3a0a38b26ab133cd257fd45c
johnworker <78348941+johnworker@users.noreply.github.com>
2023-07-20 20:58:28 +0800

    調整

---
+++ Assets/Scripts/Template/Hiddelevel/ScoreControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreControl : MonoBehaviour
+{
+	public TextMeshProUGUI scoreText;
+	private int originalScore = 0;
+	
+	private void UpdateScoreUI()
+	{
+		scoreText.text = "Score" + originalScore.ToString();
+	}
+}
