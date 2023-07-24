ecc2cdd5f5553233af4a0ef82788fabc76910db3
hunte <besscomdark@gmail.com>
2023-07-24 23:05:52 +0800

    調整隱藏關卡

---
+++ Assets/Scripts/test/ChangeAlbedoColor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class ChangeAlbedoColor : MonoBehaviour
+{
+    public Color initialColor = Color.yellow; // 初始的Albedo颜色
+
+    private Renderer objectRenderer; // 当前物体的渲染器组件
+    private Material originalMaterial; // 原始材质
+    private Color originalColor; // 原始Albedo颜色
+
+    private void Start()
+    {
+        // 获取当前物体的渲染器组件
+        objectRenderer = GetComponent<Renderer>();
+
+        // 保存原始材质和Albedo颜色
+        originalMaterial = objectRenderer.material;
+        originalColor = originalMaterial.color;
+
+        // 将初始颜色设置给目标物体
+        SetAlbedoColor(initialColor);
+    }
+
+    // 设置Albedo颜色
+    public void SetAlbedoColor(Color color)
+    {
+        // 克隆原始材质
+        Material newMaterial = new Material(originalMaterial);
+
+        // 设置新的Albedo颜色
+        newMaterial.color = color;
+
+        // 将新的材质设置为物体的材质
+        objectRenderer.material = newMaterial;
+    }
+
+    // 恢复原始Albedo颜色
+    public void ResetAlbedoColor()
+    {
+        objectRenderer.material.color = originalColor;
+    }
+}
---
+++ Assets/Scripts/test/HiddenLevelTimer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HiddenLevelTimer : MonoBehaviour
+{
+    public float levelDuration = 30f; // 隐藏关卡的时长，单位为秒
+    private float timer; // 当前倒计时时间
+
+    private void Start()
+    {
+        timer = levelDuration;
+    }
+
+    private void Update()
+    {
+        // 更新倒计时时间
+        timer -= Time.deltaTime;
+
+        // 检查倒计时是否结束
+        if (timer <= 0f)
+        {
+            // 返回原本场景
+            ReturnToOriginalScene();
+        }
+    }
+
+    private void ReturnToOriginalScene()
+    {
+
+        // 加载原本场景
+        SceneManager.LoadScene("測試彈珠台");
+    }
+}
