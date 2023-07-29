f5291d1b6fa1af97b556da857c7b021be38a75ab
hunte <besscomdark@gmail.com>
2023-07-29 17:44:16 +0800

    測試調整

---
+++ Assets/Scripts/Template/GameController/ButtonUnlocker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonUnlocker : MonoBehaviour
+{
+    public Button unlockButton;
+    public Image buttonImage;
+
+
+    void Start()
+    {
+        CheckAndUnlockButton();
+    }
+
+    public void CheckAndUnlockButton()
+    {
+        int finalScore = ScoreManager.GetFinalScore();
+        if (finalScore >= 500)
+        {
+            unlockButton.interactable = true; // 解锁按钮
+            buttonImage.gameObject.SetActive(false); // 隐藏按钮中的图片
+        }
+        else
+        {
+            unlockButton.interactable = false; // 锁定按钮
+            buttonImage.gameObject.SetActive(true); // 显示按钮中的图片
+        }
+    }
+}
