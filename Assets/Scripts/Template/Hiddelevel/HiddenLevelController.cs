37a48a59b636622ba63c6450a554b61e52516276
hunte <besscomdark@gmail.com>
2023-07-29 17:49:50 +0800

    隱藏關卡調整腳本

---
+++ Assets/Scripts/Template/Hiddelevel/HiddenLevelController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenLevelController : MonoBehaviour
+{
+    public Light hiddenLevelLight; // 对应隐藏关卡中的灯光
+
+    private void Start()
+    {
+        // 开启灯光
+        if (hiddenLevelLight != null)
+        {
+            hiddenLevelLight.enabled = true;
+        }
+    }
+}
