974faa0da49f9adfa9bc3daf0869eb4479492c25
hunte <besscomdark@gmail.com>
2023-07-29 17:56:52 +0800

    調整

--- Assets/Scripts/Template/Hiddelevel/HiddenLevelController.cs
+++ Assets/Scripts/Template/Hiddelevel/HiddenLevelController.cs
@@ -6,7 +6,7 @@ public class HiddenLevelController : MonoBehaviour
 {
     public Light hiddenLevelLight; // 对应隐藏关卡中的灯光
 
-    private void Start()
+    private void Awake()
     {
         // 开启灯光
         if (hiddenLevelLight != null)
