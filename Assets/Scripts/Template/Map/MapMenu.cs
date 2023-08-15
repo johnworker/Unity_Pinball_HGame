7821a2d3eb23518a25b8e5bf1ed94cb90429c0ae
hunte <besscomdark@gmail.com>
2023-08-15 16:07:40 +0800

    地圖對應預覽測試

---
+++ Assets/Scripts/Template/Map/MapMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapMenu : MonoBehaviour
+{
+    public RawImage[] mapImages;
+    public Button[] buttons;
+
+    // 設定點擊事件處理程序
+    private void Start()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int mapIndex = i; // 創建一個臨時變數以避免閉包問題
+            buttons[i].onClick.AddListener(() => ShowMap(mapIndex));
+        }
+    }
+
+    // 顯示選定的地圖
+    public void ShowMap(int mapIndex)
+    {
+        // 根據地圖索引在 mapImages 中更新對應的 RawImage
+        for (int i = 0; i < mapImages.Length; i++)
+        {
+            mapImages[i].gameObject.SetActive(i == mapIndex);
+        }
+    }
+
+}
