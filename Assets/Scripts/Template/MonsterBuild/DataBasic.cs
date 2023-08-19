f2c5a71a9d4f1d6a90a644d942779257d2d2c9fb
hunte <besscomdark@gmail.com>
2023-08-19 20:45:02 +0800

    添加物件化腳本

---
+++ Assets/Scripts/Template/MonsterBuild/DataBasic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName = "DARKNESS/Data Basic")]
+public class DataBasic : ScriptableObject
+{
+    [Header("移動速度"), Range(0, 100)]
+    public float moveSpeed;
+    [Header("攻擊速度"), Range(0, 50)]
+    public float attackSpeed;
+}
