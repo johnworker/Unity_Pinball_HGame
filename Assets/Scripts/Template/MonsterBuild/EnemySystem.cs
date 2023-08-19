955f6fe2755cc57a377b103fbcb6d94081b70910
hunte <besscomdark@gmail.com>
2023-08-19 22:38:38 +0800

    調整

---
+++ Assets/Scripts/Template/MonsterBuild/EnemySystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySystem : MonoBehaviour
+{
+    private DataBasic dataBasic;
+
+    public GameObject movePointLeft;
+
+    public Transform movePointRight;
+
+
+    public void EnemyRandomMove()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Template/MonsterBuild/SpawnSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SpawnSystem : MonoBehaviour
+{
+    [Header("生成怪物預置物")]
+    public GameObject preEnemy;
+    [Header("生成怪物間隔時間"),Range(0, 60)]
+    public float interval = 5f;
+	[Header("生成怪物預置物")]
+	public Transform spawnPoint;
+
+
+	private void SpawnEnemy()
+	{
+		Instantiate(preEnemy, spawnPoint);
+	}
+
+	private void Awake()
+	{
+		InvokeRepeating("SpawnEnemy", 3, interval);
+	}
+}
