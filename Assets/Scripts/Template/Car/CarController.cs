046eee4737a8058a10c8ea909b50a78311d2948f
hunte <besscomdark@gmail.com>
2023-09-09 11:45:45 +0800

    調整地圖

---
+++ Assets/Scripts/Template/Car/CarController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [Header("車體移動")]
+    public string parCarBodyMove = "車體移動";  // 車體移動 bool
+    [Header("右前輪滾動")]
+    public string parRightFrontRoll = "右前輪滾動";  // 右前輪滾動 bool
+    [Header("右後輪滾動")]
+    public string parRightBackRoll = "右後輪滾動";  // 右後輪滾動 bool
+    [Header("左前輪滾動")]
+    public string parLeftFrontRoll = "左前輪滾動";  // 左前輪滾動 bool
+    [Header("左後輪滾動")]
+    public string parLeftBackRoll = "左後輪滾動";  // 左後輪滾動 bool
+
+    public float forwardSpeed = 10.0f;
+    public float backwardSpeed = -5.0f;
+    public float stopDuration = 2.0f; // 停止的時間（秒）
+
+    private Animator carBodyAnimator;
+    public Animator wheelRightFrontAnimator;
+    public Animator wheelRightBackAnimator;
+    public Animator wheelLeftFrontAnimator;
+    public Animator wheelLeftBackAnimator;
+
+
+    private void Start()
+    {
+        carBodyAnimator = GetComponent<Animator>();
+
+
+        // 啟動協程開始車輛運動循環
+        StartCoroutine(CarMotionLoop());
+    }
+
+    private IEnumerator CarMotionLoop()
+    {
+        while (true)
+        {
+            // 車體移動
+            carBodyAnimator.SetBool(parCarBodyMove, true);
+            wheelRightFrontAnimator.SetBool(parRightFrontRoll, true);
+            wheelRightBackAnimator.SetBool(parRightBackRoll, true);
+            wheelLeftFrontAnimator.SetBool(parLeftFrontRoll, true);
+            wheelLeftBackAnimator.SetBool(parLeftBackRoll, true);
+
+            // 前進
+            yield return new WaitForSeconds(forwardSpeed);
+
+            // 靜止
+            carBodyAnimator.SetBool(parCarBodyMove, false); 
+            wheelRightFrontAnimator.SetBool(parRightFrontRoll, false);
+            wheelRightBackAnimator.SetBool(parRightBackRoll, false);
+            wheelLeftFrontAnimator.SetBool(parLeftFrontRoll, false);
+            wheelLeftBackAnimator.SetBool(parLeftBackRoll, false);
+
+            yield return new WaitForSeconds(stopDuration);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Template/Car/PornCar.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PornCar : MonoBehaviour
+{
+    [Header("移動")]
+    public string parCarMove = "移動";  // 車移動 bool
+    public float carSpeed = 3.0f;
+    public float stopDuration = 2.0f; // 停止的時間（秒）
+
+    private Animator carAnimator;
+
+    [Header("粉碎特效")]
+    public GameObject crushEffect;
+
+    private int hitCount = 0; // 記錄被打的次數
+    [Header("可被打擊次數")]
+    public int canHitCountNumbers = 3; // 記錄被打的次數
+
+    [SerializeField, Header("受傷爆炸音效")]
+    private AudioClip soundExplode;
+
+
+    private void Start()
+    {
+        carAnimator = GetComponent<Animator>();
+
+
+        // 啟動協程開始車輛運動循環
+        StartCoroutine(CarMotionLoop());
+    }
+
+
+    private IEnumerator CarMotionLoop()
+    {
+        while (true)
+        {
+            // 車體移動
+            carAnimator.SetBool(parCarMove, true);
+            // 前進
+            yield return new WaitForSeconds(carSpeed);
+
+            // 靜止
+            carAnimator.SetBool(parCarMove, false);
+
+            yield return new WaitForSeconds(stopDuration);
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 檢測到碰撞的對像是球時
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            // 如果被打擊兩次則播放粒子特效，角色的(子物件)衣服消失
+            hitCount++; // 增加被打的次數
+
+
+            // 如果被打次數達到，則銷毀障礙物
+            if (hitCount >= canHitCountNumbers)
+            {
+                SystemSound.instance.PlaySound(soundExplode, new Vector2(1.1f, 1.5f));
+                Destroy(gameObject);
+                DestroyEffect();
+            }
+        }
+    }
+
+    private void DestroyEffect()
+    {
+        GameObject effectIns = Instantiate(crushEffect, transform.position, transform.rotation);
+
+        Destroy(effectIns, 5f);
+    }
+
+
+}
