475787e7902668b75aa0b9f3be937d3776a19071
hunte <besscomdark@gmail.com>
2023-09-10 22:21:56 +0800

    添加物件

---
+++ Assets/Scripts/Template/Sky/CloudControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CloudControl : MonoBehaviour
+{
+    //緩緩往左移動到彈珠台左牆穿越彈珠台右牆再度出現
+    public float moveSpeed = 0.5f; // 移動速度
+    public float leftBoundary = -6f; // 左邊界位置
+    public float rightBoundary = 6f; // 右邊界位置
+
+    void Update()
+    {
+        // 移動雲朵向左
+        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+        // 如果雲朵移到了左邊界
+        if (transform.position.x < leftBoundary)
+        {
+            // 移動到右邊界再度出現
+            Vector3 newPosition = new Vector3(rightBoundary, transform.position.y, transform.position.z);
+            transform.position = newPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Template/Sky/ColumnController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ColumnController : MonoBehaviour
+{
+    public float fallSpeed = 2f; // 下降速度
+    public float spawnInterval = 2f; // 生成間隔時間（秒）
+
+    private void Start()
+    {
+        // 啟動協程以定期生成"天柱"
+        StartCoroutine(SpawnColumn());
+    }
+
+    private IEnumerator SpawnColumn()
+    {
+        while (true)
+        {
+            // 在一定時間後生成"天柱"
+            yield return new WaitForSeconds(spawnInterval);
+
+            // 生成"天柱"的新實例，位置在屏幕上方以外
+            Vector3 spawnPosition = new Vector3(Random.Range(-4f, 4f), 10f, 0f);
+            Instantiate(gameObject, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+        // 讓"天柱"向下移動
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+    }
+
+    // 在Scene視圖中繪製Gizmos
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(1f, 1f, 1f)); // 顯示"天柱"的邊界框
+    }
+
+}
