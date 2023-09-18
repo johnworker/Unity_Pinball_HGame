4a2b88f52efcc2f1ccac1b562e5bd3f7a1db2ffc
hunte <besscomdark@gmail.com>
2023-09-19 07:01:28 +0800

    拉近拉遠鏡頭功能

---
+++ Assets/Scripts/Template/Function/RotateOnMouseDrag.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class RotateOnMouseDrag : MonoBehaviour
+{
+    private Vector3 dragOrigin;
+    private Vector3 offset;
+    private bool isDragging = false;
+
+    void Update()
+    {
+        // 检测鼠标左键的按下和抬起
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 记录拖曳的起始点
+            dragOrigin = Input.mousePosition;
+            isDragging = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        // 如果正在拖曳，计算鼠标移动的距离并旋转场景
+        if (isDragging)
+        {
+            Vector3 currentMousePosition = Input.mousePosition;
+            offset = currentMousePosition - dragOrigin;
+
+            // 旋转场景，这里可以根据需求调整旋转速度
+            float rotationSpeed = 1.0f;
+            transform.Rotate(Vector3.up * -offset.x * rotationSpeed, Space.World);
+
+            // 更新拖曳的起始点
+            dragOrigin = currentMousePosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Template/Function/ZoomOnMouseDrag.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ZoomOnMouseDrag : MonoBehaviour
+{
+    private Vector3 dragOrigin;
+    private float dragSpeed = 0.1f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentMousePosition = Input.mousePosition;
+            Vector3 dragDelta = currentMousePosition - dragOrigin;
+
+            // 计算缩放值并应用于摄影机的位置
+            float zoomAmount = dragDelta.y * dragSpeed;
+            Camera.main.transform.Translate(0, 0, zoomAmount);
+
+            // 更新拖动的起始点
+            dragOrigin = currentMousePosition;
+        }
+    }
+}
