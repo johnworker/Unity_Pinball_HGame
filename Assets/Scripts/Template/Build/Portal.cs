fc371cd6ce89818774c4a0dc48001eda3362f435
hunte <besscomdark@gmail.com>
2023-09-18 22:16:13 +0800

    隱藏關卡完成

---
+++ Assets/Scripts/Template/Build/Portal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [Header("速度")]
+    public float speed = 1f;
+
+    private void Update()
+    {
+        transform.Rotate(0f, 5f * speed * Time.deltaTime, 0f);
+    }
+}
