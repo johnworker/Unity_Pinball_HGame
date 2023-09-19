e7eaca44fe4729b180fe791f108d8b9ec688b93a
johnworker <78348941+johnworker@users.noreply.github.com>
2023-09-19 20:43:52 +0800

    調整

---
+++ Assets/Scripts/Template/MosaicEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MosaicEffect : MonoBehaviour
+{
+    public Material mosaicMaterial; // 马赛克材质球
+
+    void Start()
+    {
+        ApplyMosaicEffect();
+    }
+
+    void ApplyMosaicEffect()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+
+        if (mosaicMaterial != null && renderer != null)
+        {
+            renderer.material = mosaicMaterial;
+        }
+        else
+        {
+            Debug.LogError("Mosaic material or renderer not found.");
+        }
+    }
+}
